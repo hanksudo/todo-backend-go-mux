a5473d42662d6458035e85e04125a19f9dc68809
Hank Wang <467745+hanksudo@users.noreply.github.com>
2022-11-15 23:41:11 +0900

    add main file

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.Use(loggingMiddleware)
+
+	r.HandleFunc("/", listTodosHandler).Methods(http.MethodGet)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
