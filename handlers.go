89b0b5303e895515e760d3a6d7ee2edf3b42e795
Hank Wang <467745+hanksudo@users.noreply.github.com>
2022-11-16 22:30:22 +0900

    add getTodo

--- handlers.go
+++ handlers.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 var todoRespository = NewTodoRepository()
@@ -14,6 +17,21 @@ func listTodosHandler(w http.ResponseWriter, r *http.Request) {
 
 	todos := todoRespository.GetAll()
 	json.NewEncoder(w).Encode(todos)
+}
+
+func getTodoHandler(w http.ResponseWriter, r *http.Request) {
+	idVar := mux.Vars(r)["id"]
+
+	id, err := strconv.Atoi(idVar)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	todo := todoRespository.Get(id)
+	json.NewEncoder(w).Encode(todo)
 }
 
 func createTodoHandler(w http.ResponseWriter, r *http.Request) {
--- main.go
+++ main.go
@@ -12,10 +12,10 @@ func main() {
 	r.Use(loggingMiddleware)
 
 	r.HandleFunc("/todos", listTodosHandler).Methods(http.MethodGet)
+	r.HandleFunc("/todos/{id:[0-9]+}", getTodoHandler).Methods(http.MethodGet)
 	r.HandleFunc("/todos", createTodoHandler).Methods(http.MethodPost)
 	// r.HandleFunc("/todos", deleteAllTodosHandler).Methods(http.MethodDelete)
 
-	// r.HandleFunc("/todos", getTodoHandler).Methods(http.MethodGet)
 	// r.HandleFunc("/todos", deleteTodoHandler).Methods(http.MethodDelete)
 	// r.HandleFunc("/todos", updateTodoHandler).Methods(http.MethodPatch)
 
